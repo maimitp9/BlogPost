8a511dd2b1d45dac489930c5fdd53c0399f6ab76
maimitp9@gmail.com <maimitp9@gmail.com>
2016-03-16 17:05:23 +0530

    Add Comment page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
 
 	def create
-		@post = Post.find(params[:id])
+		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment].permit(:name, :body))
-		redirect_to post_path
+		redirect_to post_path(@post)
 	end
 end
---
+++ db/migrate/20160316111859_add_clumn_ro_comment.rb
@@ -0,0 +1,5 @@
+class AddClumnRoComment < ActiveRecord::Migration
+  def change
+  	add_column :comments, :post_id, :integer
+  end
+end
